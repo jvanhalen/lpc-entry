4eeedec4e48e75fb4552f3207cbe4479750e6c2a
moose3d <entity@iki.fi>
2012-09-02 00:38:18 +0300

    AI game registration and challenge reply logic added.

--- server/ai.js
+++ server/ai.js
@@ -1,27 +1,61 @@
+var configs = require('../json/configs.json');
+/* handles messages from PARENT */ 
 process.on('message', function(message) {
 
     switch(message.name) {
-      case 'AI_MESSAGE_EXAMPLE':
-        // Route AI related messages from the client to the ai child process
-        console.log("CHILD received message from PARENT:", message);
-        // Perform some magic tricks
-        ai.doSomeHandling(message);
+       case 'AI_INIT':
+        ai.init();
         break;
-      default:
-        console.log("ai message:", message);
+    case 'CHALLENGE_REQ':
+        ai.replyChallenge(message,true); // always accept challenge
+        break;
+    default:
+        ai.handleMessage(message);
     }
 
 });
 
 var ai = {
 
-  doSomeHandling: function(message) {
+    teams: [], /* player teams */
+    
+    init: function(){
+        for( i in configs.npcs ){
+            console.log("Registering computer team: "+ configs.npcs[i]);
+        }
+        process.send(JSON.stringify({type:"LOGIN_REQ", name:"LOGIN_REQ",data:{username:"Morons"}}));
+    },
+    
+    /* Registers a game for AI to be handled.  */
+    registerPlayerToGame: function(battleid) {
+    
+    },
 
-    // Do some message handling here
-    /* ... */
+    
 
-    // Send message back to PARENT process
-    process.send({type: 2, name: 'SOME_RESPONSE', response: 'Thank you for your ' + message.params.name});
+    replyChallenge: function(msg,reply)
+    {
+       process.send(JSON.stringify({
+           type:"CHALLENGE_RES", 
+           name:"CHALLENGE_RES", 
+           data:{
+               username:"Morons",
+               challenger:msg.challenger,
+               response:(reply == true ? "OK" : "NOK")
+           }
+       }));
+
+    },
+    
+    handleMessage: function(message) {
+        console.log('ai is handling message ' + JSON.stringify(message.name));
+        
+        
+        // Do some message handling here
+        /* ... */
+
+        // Send message back to PARENT process
+        //process.send(JSON.stringify({type:"SOME_RESPONSE", name: "SOME_RESPONSE", data: "Thank you for your " + message.name}));
 
   }
 }
