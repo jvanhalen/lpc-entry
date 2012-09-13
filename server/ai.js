0ff9d237447f74e846b7fe7b718d8e29a57ae017
moose3d <entity@iki.fi>
2012-09-14 01:33:16 +0300

    entering arena fixed for AI

--- server/ai.js
+++ server/ai.js
@@ -11,13 +11,17 @@ process.on('message', function(message) {
 
         case 'CHALLENGE_REQ':
             ai.replyChallenge(message,true); // always accept challenge, add random delay before accepting? Should people know they af
+            ai.selectBattleTeam('Morons', ["Sesir"]);
             break;
 
         case 'CREATE_USER_RESP':
             process.send(JSON.stringify({type:"LOGIN_REQ", name:"LOGIN_REQ",data:{username: message.username, "password": aiPassword }}));
             break;
 
-        default:
+        case 'WAKE_UP':
+           ai.enterArena('Morons', message.ingame);
+        break;
+    default:
             ai.handleMessage(message);
     }
 
@@ -38,6 +42,28 @@ var ai = {
     /* Registers a game for AI to be handled.  */
     registerPlayerToGame: function(aiPlayer, battleid) {
         teams[aiPlayer] = battleid;
+    },
+
+    selectBattleTeam: function(uname, names){
+
+        // pretty straightforward and crude battle team selection logic. Only single ai.
+        var msg = {
+            username: uname,
+            password: "pass",
+            gladiators: []
+        }
+        for ( var i = 0;i<4;i++){
+            if ( names[i] !== undefined )
+                msg.gladiators.push(names[i]);
+        }
+        
+        process.send( JSON.stringify({type:"BATTLETEAM_SELECT_REQ", name:"BATTLETEAM_SELECT_REQ",data: msg}));
+    },
+    
+    enterArena: function(uname, battleid) {
+        console.log('AI entering arena as ' + uname);
+        // enter arena, only single ai.
+        process.send(JSON.stringify({type:'ENTER_ARENA_REQ', name:'ENTER_ARENA_REQ', data: {username: uname, ingame: battleid}}));
     },
 
     replyChallenge: function(msg,reply)
