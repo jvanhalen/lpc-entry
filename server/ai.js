36fbc4aaa7d3bc446bd5edc9654cfce21a3ef1a7
moose3d <entity@iki.fi>
2012-09-23 22:37:06 +0300

    MOVE_UPDATE reflects changes to internal structures. BATTLE_START updates spatial map. isEnemy() added. getGladiatorByName() added. update() checks surroundings and attacks enemies on left side. proper registering of ai players when ENTER_ARENA_REQ is sent.

--- server/ai.js
+++ server/ai.js
@@ -21,15 +21,27 @@ process.on('message', function(message) {
         case 'WAKE_UP':
            ai.enterArena('Morons', message.ingame);
         break;
+        case 'MOVE_UPDATE':
+           ai.handleMoveUpdate(message);
+        break;
+        case 'UPDATE':
+           ai.update(message.tick);
+        break;
+        case 'BATTLE_START':
+           ai.handleBattleStart(message);
+        break;
     default:
-            ai.handleMessage(message);
+          ai.handleMessage(message);
     }
 
 });
 
+
+
 var ai = {
 
     teams: {}, /* player teams */
+    
     
     init: function(){
 
@@ -38,10 +50,155 @@ var ai = {
             process.send(JSON.stringify({type:"CREATE_USER_REQ", name:"CREATE_USER_REQ",data:{ username:configs.npcs[i], "password": aiPassword, "ai": true }}));
         }
     },
+    
+    handleMoveUpdate: function(msg)
+    {
+        console.log('AI is handling move update...');
+        for( var ai in this.teams ){
+            if ( this.teams[ai].ingame == msg.battleid )
+            {
+                if ( msg.username == this.teams[ai].battle.challenger.name ) {
+                    
+                    var gladiators = this.teams[ai].battle.challenger.gladiators;
+                    for( var g in gladiators)
+                    {
+                        if ( gladiators[g].name == msg.gladiator )
+                        {
+                            console.log('Gladiator',  gladiators[g].name, "now in", msg.pos );
+
+                            var pos = gladiators[g].battledata.pos;
+                            this.teams[ai].battle.map[pos[1]][pos[0]] = 0;
+                            pos = gladiators[g].battledata.pos = msg.pos;
+                            this.teams[ai].battle.map[pos[1]][pos[0]] = gladiators[g].name;
+                        }
+                    }
+                } else if ( msg.username == this.teams[ai].battle.defender.name ) {
+                    
+                    var gladiators = this.teams[ai].battle.defender.gladiators;
+                    for( var g in gladiators)
+                    {
+                        if ( gladiators[g].name == msg.gladiator )
+                        {
+                            console.log('Gladiator',  gladiators[g].name, "now in", msg.pos );
+                            var pos = gladiators[g].battledata.pos;
+                            this.teams[ai].battle.map[pos[1]][pos[0]] = 0;
+                            pos = gladiators[g].battledata.pos = msg.pos;
+                            this.teams[ai].battle.map[pos[1]][pos[0]] = gladiators[g].name;
+                        }
+                    }
+                }
+            }
+        }
+    },
+
+    handleBattleStart: function(battle)
+    {
+        console.log('AI received BATTLE_START:' /*+ data[0]*/);
+        
+        // convert battle map into spatial positioning map
+        for(var g in battle.challenger.gladiators)
+        {
+            for( var bg in battle.challenger.battleteam ){
+                var gladiator = battle.challenger.gladiators[g];
+                if ( gladiator.name == battle.challenger.battleteam[bg] )
+                {
+                    var row = gladiator.battledata.pos[1];
+                    var column = gladiator.battledata.pos[0];
+                    battle.map[row][column] = gladiator.name;
+                }
+            }
+        }
+
+        for(var g in battle.defender.gladiators)
+        {
+            for( var bg in battle.defender.battleteam ){
+                var gladiator = battle.defender.gladiators[g];
+                if ( gladiator.name == battle.defender.battleteam[bg] )
+                {
+                    var row = gladiator.battledata.pos[1];
+                    var column = gladiator.battledata.pos[0];
+                    battle.map[row][column] = gladiator.name;
+                }
+            }
+        }
+        // store battle into ai team property
+        this.teams[battle.defender.name]["battle"] = battle;
+    },
+
+    // AI enemy will always be a challenger.
+    isEnemy: function( battleid, gladiatorname )
+    {
+        for( var ai in this.teams ){
+            if ( this.teams[ai].ingame == battleid )
+            {
+                for( var g in this.teams[ai].battle.challenger.gladiators)
+                {
+                    if ( this.teams[ai].battle.challenger.gladiators[g].name == gladiatorname)
+                        return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    getGladiatorByName: function(ai, gladiatorname)
+    {
+        for(var g in ai.battle.defender.gladiators)
+        {
+            if ( ai.battle.defender.gladiators[g].name == gladiatorname ) 
+                return ai.battle.defender.gladiators[g];
+        }
+
+        for(var g in ai.battle.challenger.gladiators)
+        {
+            if ( ai.battle.challenger.gladiators[g].name == gladiatorname ) 
+                return ai.battle.challenger.gladiators[g];
+        }
+
+        return null;
+    },
+   
+
+    update: function(tick){
+
+        for( var ai in this.teams ){
+            console.log('Seeking enemies2...');
+            if ( this.teams[ai].battle === undefined ) continue;
+
+            var team = this.teams[ai].battle.defender.battleteam;
+            for( var bt in team)
+            {
+                var g = this.getGladiatorByName( this.teams[ai], team[bt] );
+                var target = this.teams[ai].battle.map[g.battledata.pos[1]][g.battledata.pos[0]-1];
+                
+                if ( target != 0 && target != 1 ){
+
+                    if ( this.isEnemy(this.teams[ai].ingame, target) )   
+                    {
+                        console.log('AI found an enemy nearby!', target);
+                        var attackMsg = {
+                            type: "ATTACK_REQ",
+                            name: "ATTACK_REQ",
+                            username: this.teams[ai].name,
+                            attackerid: g.name,
+                            targetid: target,
+                            battleid: this.teams[ai].ingame
+                        }
+                        
+                        process.send( JSON.stringify({name: "ATTACK_REQ", type: "ATTACK_REQ", data: attackMsg}) );
+                    }
+                    else {
+                        console.log('Something there, but its our own', name);
+                    }
+
+                }
+            }
+        }
+    },
 
     /* Registers a game for AI to be handled.  */
     registerPlayerToGame: function(aiPlayer, battleid) {
-        teams[aiPlayer] = battleid;
+        this.teams[aiPlayer] = { ingame: battleid, name: aiPlayer };
     },
 
     selectBattleTeam: function(uname, names){
@@ -64,6 +221,7 @@ var ai = {
         console.log('AI entering arena as ' + uname);
         // enter arena, only single ai.
         process.send(JSON.stringify({type:'ENTER_ARENA_REQ', name:'ENTER_ARENA_REQ', data: {username: uname, ingame: battleid}}));
+        this.registerPlayerToGame(uname, battleid);
     },
 
     replyChallenge: function(msg,reply)
